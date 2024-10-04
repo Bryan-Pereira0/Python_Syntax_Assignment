6578d30e344ea6cb804c7d3e6d1d03af82913da9
bryanpereira <gfxmagicz@gmail.com>
2024-10-03 23:55:46 -0400

    Finished both tasks

---
+++ Showcase_Your_Dance_Moves.py
@@ -0,0 +1,14 @@
+#1. Showcase your dance moves!
+#Task1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task2
+Greeting = input("How are you today?")
+    
+if Greeting == ("happy"or"sad"):
+    print("That's awesome to hear!"),("Bummer, hope your day gets better :)")
