a752bd32c9595354f08f29db9c9b8872c3b75ae7
bryanpereira <gfxmagicz@gmail.com>
2024-10-04 00:16:19 -0400

    finished task 2

--- Arithmetic_Operations_In_Daily_Life.py
+++ Arithmetic_Operations_In_Daily_Life.py
@@ -4,6 +4,10 @@ bread = 100
 peanut_butter = 5
 jelly = 3000
 receipt = jelly+peanut_butter+bread
-print(receipt)
+print(receipt,"This is your grocery store total.")
 #Task2
+savings = 10000
+interest = 7
+savings_at_end_of_year = (interest/100)*savings+savings
+print(savings_at_end_of_year,"This is your savings after interest.")
 
